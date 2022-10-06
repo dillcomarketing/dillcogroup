c9625fc7f7ac8181c875a22727a6b92fa793e31d
Luthfi Tri Atmaja <luthfi.personal@gmail.com>
2022-10-07 03:32:46 +0700

    implement dci slicing

---
+++ source/_assets/js/pages/dillco-chocolate.js
@@ -0,0 +1,6 @@
+import initEmbla from '../embla';
+
+initEmbla('#dillco-chocolate-banners .embla', { autoPlay: true, loop: true, startIndex: 0 });
+initEmbla('#lineup-slider-classic-series .embla', { loop: true, startIndex: 1 });
+initEmbla('#lineup-slider-fine-choco-series .embla', { loop: true, startIndex: 1 });
+initEmbla('#lineup-slider-3in1-series .embla', { loop: true, startIndex: 1 });
--- source/_components/video-thumb.blade.php
+++ source/_components/video-thumb.blade.php
@@ -1,6 +1,6 @@
 <div class="relative {{ $size == 'large' ? 'w-96 h-96' : 'w-72 h-72' }} flex-grow-0 flex-shrink-0 bg-white">
     <div class="flex items-center justify-center w-full h-full absolute">
-        <div class="flex items-center justify-center {{ $size == 'large' ? 'w-24 h-24' : 'w-20 h-20' }} bg-flavorich rounded-full cursor-pointer hover:opacity-80 shadow-lg">
+        <div class="flex items-center justify-center {{ $size == 'large' ? 'w-24 h-24' : 'w-20 h-20' }} {{ $buttonClasses ?? 'bg-black' }} rounded-full cursor-pointer hover:opacity-80 shadow-lg">
             <div class="ml-1">
                 <svg xmlns="http://www.w3.org/2000/svg"
                     fill="white"
