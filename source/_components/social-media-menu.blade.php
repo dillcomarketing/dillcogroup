92f4007f6efb14f9015ff3cec333b38724583fcf
Luthfi Tri Atmaja <luthfi.personal@gmail.com>
2022-10-07 04:51:56 +0700

    refactor homepage

---
+++ source/_components/social-media-menu.blade.php
@@ -0,0 +1,12 @@
+<ul id="social-media-navigation" role="navigation" class="flex items-center {{ $classes }}">
+    <li class="mr-2 hover:opacity-70 {{ $listClasses }}">
+        <a href="#">
+            <x-icons.linkedin size="{{ $iconSize ?? 18 }}" />
+        </a>
+    </li>
+    <li class="mr-2 hover:opacity-70 {{ $listClasses }}">
+        <a href="#">
+            <x-icons.instagram size="{{ $iconSize ?? 18 }}" />
+        </a>
+    </li>
+</ul>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,26 +1,39 @@
 @php
-$banners = ['/assets/images/index/banner-dilltea.png', '/assets/images/index/banner-dci.png', '/assets/images/index/banner-flavorich.png'];
-$brands = ['/assets/images/index/dci.png', '/assets/images/index/fck.png', '/assets/images/index/dilltea.png', '/assets/images/index/kopsil.png', '/assets/images/index/palmrich.png', '/assets/images/index/gmn.png'];
+
+$menus = [
+    [ 'title' => 'home', 'link' => '/', 'active' => true ],
+    [ 'title' => 'about', 'link' => '#about' ],
+    [ 'title' => 'brands', 'link' => '#brands' ],
+    [ 'title' => 'catalog', 'link' => '#brands' ],
+    [ 'title' => 'contact', 'link' => '#contact' ],
+];
+
+$banners = [
+    '/assets/images/index/banner-dilltea.png',
+    '/assets/images/index/banner-dci.png',
+    '/assets/images/index/banner-flavorich.png'
+];
+
+$brands = [
+    '/assets/images/index/dci.png',
+    '/assets/images/index/fck.png',
+    '/assets/images/index/dilltea.png',
+    '/assets/images/index/kopsil.png',
+    '/assets/images/index/palmrich.png',
+    '/assets/images/index/gmn.png'
+];
+
 @endphp
 
 @extends('_layouts.blank')
 
 @section('body')
+
+    {{-- header --}}
     <header class="flex items-center p-2 px-4 lg:px-8">
-        <ul class="flex items-center">
-            <li class="mr-2 hover:opacity-70">
-                <a href="#">
-                    <x-icons.linkedin size="18" />
-                </a>
-            </li>
-            <li class="mr-2 hover:opacity-70">
-                <a href="#">
-                    <x-icons.instagram size="18" />
-                </a>
-            </li>
-        </ul>
+        <x-social-media-menu />
         <div class="flex-1 text-center">
-            <a href="#" class="inline-block text-xl font-semibold hover:opacity-70">
+            <a href="/" class="inline-block text-xl font-semibold hover:opacity-70">
                 <h1 title="Dillco Group">
                     <span class="inline-block bg-yellow-400">d</span>illco group
                 </h1>
@@ -33,29 +46,22 @@ $brands = ['/assets/images/index/dci.png', '/assets/images/index/fck.png', '/ass
         </div>
     </header>
 
-    <nav>
+    {{-- site menus --}}
+    <nav role="navigation">
         <ul class="flex justify-evenly items-center p-2 border-t border-b border-gray-800 uppercase text-sm font-semibold">
-            <li class="border-b-4 border-yellow-400 hover:opacity-70 mx-1">
-                <a href="#">Home</a>
-            </li>
-            <li class="border-b-4 border-transparent hover:opacity-70 mx-1">
-                <a href="#">About</a>
-            </li>
-            <li class="border-b-4 border-transparent hover:opacity-70 mx-1">
-                <a href="#">Brands</a>
-            </li>
-            <li class="border-b-4 border-transparent hover:opacity-70 mx-1">
-                <a href="#">Catalog</a>
-            </li>
-            <li class="border-b-4 border-transparent hover:opacity-70 mx-1">
-                <a href="#">Contact</a>
-            </li>
+            @foreach ($menus as $menu)    
+                <li class="border-b-4 hover:opacity-70 hover:border-yellow-400 mx-1 {{ $menu['active'] ? 'border-yellow-400' : 'border-transparent' }}">
+                    <a href="{{ $menu['link'] }}" title="{{ ucwords($menu['title']) }}">{{ $menu['title'] }}</a>
+                </li>
+            @endforeach
         </ul>
     </nav>
-
+    
+    {{-- main container --}}
     <main class="mx-auto max-w-screen-xl-768 py-2">
         <div class="mx-6">
 
+            {{-- banner slider --}}
             <section id="banners" class="mb-4 md:mb-8 -mx-2 sm:mx-0">
                 <x-embla-images :images="$banners" classes='embla--master-view'>
                     <button class="embla__prev-button absolute left-5 ml-2 top-45">
@@ -65,15 +71,16 @@ $brands = ['/assets/images/index/dci.png', '/assets/images/index/fck.png', '/ass
                         <x-icons.next size="24" fill='white' />
                     </button>
                     <div class="absolute top-65 xs:top-70 md:top-80 xl:top-85 right-45 md:right-50">
-                        <button class="embla__dot-button mx-1"></button>
-                        <button class="embla__dot-button mx-1"></button>
-                        <button class="embla__dot-button mx-1"></button>
+                        @for ($i = 0; $i < count($banners); $i++)
+                            <button class="embla__dot-button mx-1"></button>    
+                        @endfor
                     </div>
                 </x-embla-images>
             </section>
 
-            <section id="about-us" class="mb-4">
-                <h2 class="uppercase text-4xl font-bold tracking-wide mb-4">
+            {{-- about --}}
+            <section id="about" class="mb-4">
+                <h2 class="uppercase text-4xl font-bold tracking-wide mb-4" title="Tentang Kami">
                     <span class="border-b-4 border-transparent">Tentang</span>
                     <span class="border-b-4 border-yellow-400">Kami</span>
                 </h2>
@@ -94,47 +101,43 @@ $brands = ['/assets/images/index/dci.png', '/assets/images/index/fck.png', '/ass
                             dengan harga terjangkau. Dillco Group siap membantu meningkatkan kualitas usaha Anda!
                         </p>
                     </div>
-                    <div class="bg-pa-fadil hidden md:block lg:w-1/2 -my-32">
-                    </div>
+                    <span class="bg-pa-fadil hidden md:block lg:w-1/2 -my-32"></span>
                 </div>
             </section>
 
+            {{-- brand slider --}}
             <section id="brands" class="mt-8 lg:mt-32 mb-8">
                 <x-embla-images :images="$brands" slideClasses="mx-1" />
             </section>
-
-            <section id="join-us" class="mb-8">
-                <h2 class="text-center uppercase text-4xl tracking-wide font-bold mb-5">
-                    <span class="border-b-4 border-yellow-400">Join</span>
-                    <span class="border-b-4 border-transparent">With Us</span>
-                </h2>
-                <ul class="flex items-center justify-center mb-8">
-                    <li class="mr-3 hover:opacity-70">
-                        <a href="#">
-                            <x-icons.linkedin size="32" />
-                        </a>
-                    </li>
-                    <li class="mr-3 hover:opacity-70">
-                        <a href="#">
-                            <x-icons.instagram size="32" />
-                        </a>
-                    </li>
-                </ul>
-                <div class="flex flex-wrap justify-between">
-                    <div class="m-4">
-                        <h2 class="text-4xl font-semibold uppercase">Offline Store</h2>
-                        <span class="text-sm">Jl. Laswi No. 1E, Bandung 40272</span>
-                    </div>
-                    <div class="m-4">
-                        <h2 class="text-4xl font-semibold uppercase">Office</h2>
-                        <span class="text-sm">Jl. Yupiter Utama No. 106, Bandung 40286</span>
-                    </div>
-                    <div class="m-4">
-                        <h2 class="text-4xl font-semibold uppercase">Phone</h2>
-                        <span class="text-sm">+62 811-2233-3761</span>
-                    </div>
-                </div>
-            </section>
         </div>
     </main>
+
+    {{-- footer --}}
+    <footer class="mx-auto max-w-screen-xl-768">
+
+        {{-- contact --}}
+        <section id="contact" class="mb-8" title="Join With Us">
+            <h2 class="text-center uppercase text-4xl tracking-wide font-bold mb-5">
+                <span class="border-b-4 border-yellow-400">Join</span>
+                <span class="border-b-4 border-transparent">With Us</span>
+            </h2>
+            <x-social-media-menu iconSize="32" classes="justify-center mb-8" listClasses="mr-3" />
+            <div class="flex flex-wrap justify-between">
+                <div class="m-4">
+                    <h3 class="text-4xl font-semibold uppercase" title="Offline Store">Offline Store</h3>
+                    <span class="text-sm">Jl. Laswi No. 1E, Bandung 40272</span>
+                </div>
+                <div class="m-4">
+                    <h3 class="text-4xl font-semibold uppercase" title="Office">Office</h3>
+                    <span class="text-sm">Jl. Yupiter Utama No. 106, Bandung 40286</span>
+                </div>
+                <div class="m-4">
+                    <h3 class="text-4xl font-semibold uppercase" title="Phone">Phone</h3>
+                    <a href="tel:+6281122333761" class="text-sm hover:underline text-flavorich">
+                        +62 811-2233-3761
+                    </a>
+                </div>
+            </div>
+        </section>
+    </footer>
 @endsection
